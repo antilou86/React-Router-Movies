8871062943da2b976eef8be91e00eefff979a737
Lou <antilou86@gmail.com>
2019-07-22 16:50:37 -0400

    stretch goal 1 completed- needed to add the returned elements in a JSX fragment.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -26,9 +26,11 @@ const MovieList = (props) => {
     <div className="movie-list">
       
       {movies.map(movie => (
+        <>
         <Link to={`/movies/${movie.id}`}>
         <MovieCard key={movie.id} movie={movie} />
         </Link>
+        </>
       ))}
       
     </div>
